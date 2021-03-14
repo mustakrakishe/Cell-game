a28a87ca1b6df458e44efbb6898f53f9c1b082bd
mustakrakishe <mustakrkish@gmail.com>
2021-03-14 16:26:47 +0200

    The field is filling by points.

--- script.js
+++ script.js
@@ -15,9 +15,20 @@ field.fillStyle = '#eee';
 field.cellPerSide = 10;
 field.cells = [];
 
+// generate the available point bases array
+points = [];
+
+for(let cellNum = 0; cellNum < field.cellPerSide/2; cellNum++) {
+    points.push((cellNum + 1) * 10);
+}
+
 // fill the field cells array
 for(let r = 0; r < field.cellPerSide; r++) {
     let r_odd = r%2;
+    let rowPositivePoints = points.slice();
+    let rowNegativePoints = points.slice().map(val => {
+        return -val;
+    });
     let row = [];
 
     for(let c = 0; c < field.cellPerSide; c++) {
@@ -28,15 +39,32 @@ for(let r = 0; r < field.cellPerSide; r++) {
         cell.x = field.x + cell.width * c;
         cell.y = field.y + cell.height * r;
 
+
         if((!r_odd && !c_odd) || (r_odd && c_odd)){
-            console.log(r);
             cell.strokeStyle = 'rgb(21, 182, 21)';
             cell.fillStyle = 'rgb(21, 182, 21)'
+
+            let random = randomInteger(0, rowPositivePoints.length - 1);
+            cell.point = rowPositivePoints.splice(random, 1);
         }
         else{
             cell.strokeStyle = 'rgb(255, 140, 140)';
             cell.fillStyle = 'rgb(255, 140, 140)'
+
+            
+            let random = randomInteger(0, rowNegativePoints.length - 1);
+            cell.point = rowNegativePoints.splice(random, 1);
         }
+
+        cell.content = {
+            'text': cell.point | 0,
+            'font': '20px Arial',
+            'fillStyle': 'white',
+            'x': cell.x,
+            'y': cell.y
+        };
+        cell.content.x = cell.x + cell.width/2 - ctx.measureText(cell.content.text).width;
+        cell.content.y = cell.y + cell.height/2 + 7;
 
         row.push(cell);
     }
@@ -53,9 +81,14 @@ function draw() {
     for(r = 0; r < field.cellPerSide; r++) {
         for(c = 0; c < field.cellPerSide; c++) {
             painter.draw(field.cells[r][c]);
+            painter.draw(field.cells[r][c].content);
         }
     }
     
 }
 
 draw();
+
+function randomInteger(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+}
